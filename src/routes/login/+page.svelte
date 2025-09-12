9b1c51839690888a0bdccc6433456964b8a2f0e3
zachczx <zixian.chen@gmail.com>
2025-09-12 23:12:17 +0800

    Update +page.svelte
    Adjusted login ui

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -36,36 +36,21 @@
 </script>
 
 <PageWrapper title="Login" {pb}>
-	<form class="grid w-full max-w-sm content-center">
-		<div class="mb-12">
-			<h1 class="text-primary text-9xl font-bold">Towsey</h1>
-			<!-- <h2 class="flex items-center gap-2 text-2xl">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					width="1em"
-					height="1em"
-					class="material-symbols:subdirectory-arrow-right-rounded"
-					viewBox="0 0 24 24"
-					><path
-						fill="currentColor"
-						d="M13.3 20.275q-.3-.3-.3-.7t.3-.7L16.175 16H7q-.825 0-1.412-.587T5 14V5q0-.425.288-.712T6 4t.713.288T7 5v9h9.175l-2.9-2.9q-.3-.3-.288-.7t.288-.7q.3-.3.7-.312t.7.287L19.3 14.3q.15.15.212.325t.063.375t-.063.375t-.212.325l-4.575 4.575q-.3.3-.712.3t-.713-.3"
-					/></svg
-				>Login
-			</h2> -->
-		</div>
+	<form class="grid h-full w-full max-w-sm content-center justify-self-center">
+		<h1 class="text-primary mb-4 text-center text-5xl font-bold lg:mb-12 lg:text-9xl">Towsey</h1>
 
 		<fieldset class="fieldset">
 			<input
 				type="text"
 				name="email"
 				bind:value={email}
-				class="input w-full"
+				class="input input-lg w-full"
 				placeholder="Email address"
 			/>
 		</fieldset>
 
 		<fieldset class="fieldset">
-			<label class="input validator w-full gap-4">
+			<label class="input validator input-lg w-full gap-4">
 				<input
 					type={togglePasswordStatus ? 'text' : 'password'}
 					bind:value={password}
@@ -74,14 +59,14 @@
 				/>
 				<button type="button" class="cursor-pointer" onclick={togglePassword}>
 					{#if togglePasswordStatus}
-						<MaterialSymbolsVisibilityOffOutline class="h-[1em] opacity-75" />
+						<MaterialSymbolsVisibilityOffOutline class="size-[1.3em] opacity-75" />
 					{:else}
-						<MaterialSymbolsVisibilityOutline class="h-[1em] opacity-75" />
+						<MaterialSymbolsVisibilityOutline class="size-[1.3em] opacity-75" />
 					{/if}
 				</button>
 			</label>
 		</fieldset>
-		<button class="btn btn-primary mt-4" onclick={() => submitHandler()}>
+		<button class="btn btn-lg btn-primary mt-4" onclick={() => submitHandler()}>
 			{#if !spinner}
 				Login
 			{:else}
