780cd76b10c79307f689b83ed2e298ffbbdbce7a
zachczx <zixian.chen@gmail.com>
2025-09-13 23:28:19 +0800

    Create +page.svelte

---
+++ src/routes/credits/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import PageWrapper from '$lib/PageWrapper.svelte';
+	import { pb } from '$lib/pb';
+</script>
+
+<svelte:head>
+	<title>Credits</title>
+</svelte:head>
+
+<PageWrapper title="Towelie" {pb}>
+	<main class="grid h-full content-center justify-items-center">
+		<ul>
+			<li>
+				Count uses content produced with Typecast, an AI virtual actor service. Characters casted:
+				Frankenstein from https://typecast.ai
+			</li>
+		</ul>
+	</main>
+</PageWrapper>
