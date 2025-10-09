879c018e675dca7d2a108a5736db819bcff6a6fb
zachczx <zixian.chen@gmail.com>
2025-10-09 22:12:59 +0800

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter() },
+	compilerOptions: {
+		experimental: {
+			async: true
+		}
+	}
 };
 
 export default config;
