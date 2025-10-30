c4f58e68609a7e7390da091a8492c719b48a5a3e
zachczx <zixian.chen@gmail.com>
2025-10-30 23:21:10 +0800

    Changed & removed icons

---
+++ src/lib/assets/towsey-icons/StreamlineKameleonColorMedicine.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	let props = $props();
+</script>
+
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	width="1em"
+	height="1em"
+	viewBox="0 0 48 48"
+	{...props}
+	class={props.class}
+	><!-- Icon from Kameleon color icons by Streamline - https://creativecommons.org/licenses/by/4.0/ --><g
+		fill="none"
+		><path
+			fill="#fabc3d"
+			d="M24 47.998c13.255 0 24-10.745 24-24C48 10.746 37.255 0 24 0S0 10.745 0 23.999s10.745 23.999 24 23.999"
+		/><path
+			fill="#059bbf"
+			d="M32.728 18.763a3.49 3.49 0 0 0-3.491-3.491H18.764a3.49 3.49 0 0 0-3.491 3.49v15.71a3.49 3.49 0 0 0 3.49 3.49h10.474a3.49 3.49 0 0 0 3.49-3.49z"
+		/><path fill="#f0f1f1" d="M32.728 20.508H15.273v12.217h17.455z" /><path
+			fill="#21b2d1"
+			d="M24 15.272v5.236h-8.727v-1.745a3.49 3.49 0 0 1 3.49-3.491z"
+		/><path fill="#fff" d="M24 20.508h-8.727v12.217H24z" /><path
+			fill="#f0f1f1"
+			d="M32.728 14.4a.873.873 0 0 1-.873.872h-15.71a.873.873 0 0 1-.872-.873v-3.49c0-.483.39-.873.873-.873h15.709c.482 0 .873.39.873.872z"
+		/><path
+			fill="#fff"
+			d="M24 10.036h-7.854a.873.873 0 0 0-.873.872V14.4c0 .482.39.873.873.873H24z"
+		/><path fill="#fa5655" d="M28.8 24.617H26v-2.8h-4v2.8h-2.8v4H22v2.8h4v-2.8h2.8z" /><path
+			fill="#f0f1f1"
+			d="M19.2 10.036h-1.309v5.236h1.31zm3.49 0h-1.308v5.236h1.309z"
+		/><path fill="#e2e4e5" d="M26.618 10.036H25.31v5.236h1.31zm3.491 0h-1.31v5.236h1.31z" /><path
+			fill="#21b2d1"
+			d="M24 32.726v5.236h-5.236a3.49 3.49 0 0 1-3.491-3.491v-1.745z"
+		/></g
+	></svg
+>
--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,17 @@ import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
-	plugins: [enhancedImages(), tailwindcss(), sveltekit()]
+	plugins: [
+		enhancedImages(),
+		tailwindcss(),
+		sveltekit(),
+		visualizer({
+			emitFile: true, // or false if you just want to open in browser
+			filename: 'bundle-analysis.html',
+			open: true // opens the visualization in the browser after build
+		})
+	]
 });
