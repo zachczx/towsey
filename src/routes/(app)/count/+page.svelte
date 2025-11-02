0a95f3a5739878b2237005305f51ad789bea788b
zachczx <zixian.chen@gmail.com>
2025-11-02 17:01:04 +0800

    Update +page.svelte

--- src/routes/(app)/count/+page.svelte
+++ src/routes/(app)/count/+page.svelte
@@ -128,7 +128,7 @@
 </svelte:head>
 
 <PageWrapper title="Count" back={true} {pb}>
-	<div class="grid h-full grid-rows-[1fr_auto]">
+	<div class="grid h-full max-w-xl grid-rows-[1fr_auto] justify-self-center">
 		<main class="grid h-full content-center justify-items-center gap-8 p-2">
 			<div class="grid w-full max-w-lg grid-cols-[auto_1fr] justify-items-center gap-y-8">
 				<div class="grid content-center opacity-75">
